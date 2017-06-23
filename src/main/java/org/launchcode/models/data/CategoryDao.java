99990a4940bb6bd0e3e56f95b9c87d685bfb4439
Katie Juhala <kajuhala@gmail.com>
2017-06-22 19:10:31 -0500

    Completed Studio part 2 plus bonus mission.

--- src/main/java/org/launchcode/models/data/CategoryDao.java
+++ src/main/java/org/launchcode/models/data/CategoryDao.java
@@ -1,18 +1,17 @@
 package org.launchcode.models.data;
 
-import org.launchcode.models.Cheese;
+import org.launchcode.models.Category;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
-import java.util.List;
 
 /**
- * Created by LaunchCode
+ * Created by kajuh_000 on 6/21/2017.
  */
+
 @Repository
 @Transactional
-public interface CheeseDao extends CrudRepository<Cheese, Integer> {
+public interface CategoryDao extends CrudRepository<Category, Integer> {
 
-    List<Cheese> findByCategoryId(Integer categoryId);
 }
